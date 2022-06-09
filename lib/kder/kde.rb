7038170a91a4b271bde988599c6b37a948e6d2e4
Benjamin Klerman <benjamin.klerman@gmail.com>
2022-06-09 14:35:41 -0400

    FIX: redudant selfs and lint issues

--- lib/kder/kde.rb
+++ lib/kder/kde.rb
@@ -17,25 +17,25 @@ class Kder
       unless bw # is nil
         bw = Bandwidth.silverman(arr)
       end
-      bw = bw == 0 ? 0.1 : bw
+      bw = bw.zero? ? 0.1 : bw
       # Initialization steps
-      range = bw*opts[:sigmas]
+      range = bw * opts[:sigmas]
       min = arr.min - range
       max = arr.max + range
-      step_size = (max-min)/(opts[:sampling_density].to_f)
+      step_size = (max - min) / opts[:sampling_density].to_f
       step_size = step_size < MinimumStepSize ? MinimumStepSize : step_size
       arr.sort!
       # initialize the range variables
       ranges = (min..max).step(step_size).to_a
-      output = [[min,0]]
+      output = [[min, 0]]
       old_intensity = 0
       # Step through the range
       ranges[1..-1].map.with_index do |mid, i|
         high_end = mid + range
         lower_end = mid - range
         selection_range = (lower_end..high_end)
-        included = arr.select {|a| selection_range.include?(a)}
-        intensity = included.map {|a| Kder::Statistics.custom_pdf(a-mid, bw) }.inject(:+) || 0
+        included = arr.select { |a| selection_range.include?(a) }
+        intensity = included.map { |a| Kder::Statistics.custom_pdf(a - mid, bw) }.inject(:+) || 0
         unless intensity < opts[:threshold] or (intensity - old_intensity).abs < opts[:minimum_delta]
           output << [mid, intensity ] 
           old_intensity = intensity
--- lib/util/enum_extensions.rb
+++ lib/util/enum_extensions.rb
@@ -2,18 +2,20 @@ module Enumerable
   # Thanks StackOverflow users!
   # http://stackoverflow.com/questions/7749568/how-can-i-do-standard-deviation-in-ruby
   def sum
-    self.inject(:+)
+    inject(:+)
   end
+
   def mean
-    self.sum/self.length.to_f
+    sum / length.to_f
   end
   alias average mean
   def sample_variance
-    m = self.mean
-    sum = self.inject(0){|accum, i| accum + (i-m)**2}
-    sum/(self.length - 1).to_f
+    m = mean
+    sum = inject(0) { |accum, i| accum + (i - m)**2 }
+    sum / (length - 1).to_f
   end
+
   def standard_deviation
-    Math.sqrt(self.sample_variance)
+    Math.sqrt(sample_variance)
   end
 end
